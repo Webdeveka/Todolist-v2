3c02a8d5122c2cf6ee46ad8c3293bd769e1ee9e1
Konstantin <webdeveka@gmail.com>
2022-11-26 18:21:11 +0300

    Modifyded some files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -11,14 +11,57 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+const itemschema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemschema);
+
+const item1 = new Item({
+  name: "Welcome to you todolist"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.deleteMany({name: 'Eat Food'}, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Your data saved is Successfully");
+//   }
+// });
+
 
 app.get("/", function(req, res) {
+// const day = date.getDate();
 
-const day = date.getDate();
+Item.find({}, (err, foundItems) => {
 
-  res.render("list", {listTitle: day, newListItems: items});
+  if (foundItems.length === 0) {
+    Item.insertMany(defaultItems, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Succesefuly the date saved to DB");
+      }
+    });
+    res.redirect("/");
+  } else {
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
+  }
+});
+
 
 });
 
@@ -26,13 +69,14 @@ app.post("/", function(req, res){
 
   const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  //не нужный код из v1
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
 
 app.get("/work", function(req,res){
